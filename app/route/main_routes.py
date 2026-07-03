5cf329379e5cc1077ff2b31299c68c4e95d7ce1e
Esteban Amores <estebanamoresbarrantes@gmail.com>
2026-07-03 10:25:01 -0600

    fix(routes): corrección de rutas de importacion de todos los archivos
    Se corrige las rutas de importacion de todos los archivos y la creación de __init__.py el cual es un archivo que le dice a Python "esta carpeta es un paquete", lo que permite importar desde ella.

--- app/route/main_routes.py
+++ app/route/main_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
-from controllers.main_controller import save_chat, refresh_bm25_index
+from controllers.main_controller import save_chat
 
 main_bp = Blueprint("main", __name__)
 
-main_bp.route("/savechat",     methods=["POST"])(save_chat)
-main_bp.route("/refresh_bm25", methods=["POST"])(refresh_bm25_index)
+main_bp.route("/savechat", methods=["POST"])(save_chat)
